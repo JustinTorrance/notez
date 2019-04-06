293c51f2c1c1d4e221eef1515995d132a80f11c1
“Kimberly <kimberly.achkio@gmail.com>
2019-04-06 13:18:19 -0600

    Add mock dispatch of isLoading(false) successful response

--- src/thunks/__tests__/fetchNotes.js
+++ src/thunks/__tests__/fetchNotes.js
@@ -18,16 +18,18 @@ describe('fetchNotes', () => {
   it('calls dispatch with isLoading', () => {
     const thunk = fetchNotes(mockUrl);
     thunk(mockDispatch);
-    expect(mockDispatch).toHaveBeenCalledWith(isLoading(true))
+    expect(mockDispatch).toHaveBeenCalledWith(isLoading(true));
   });
 
-  it('calls dispatch with getNotes if the response is ok', async () => {
+  it('calls dispatch with getNotes and isLoading(false) if the response is ok', async () => {
     window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
       ok: true,
       json: () => Promise.resolve(mockNotes)
     }));
     const thunk = fetchNotes(mockUrl);
     await thunk(mockDispatch);
-    expect(mockDispatch).toHaveBeenCalledWith(getNotes(mockNotes))
+    expect(mockDispatch).toHaveBeenCalledWith(getNotes(mockNotes));
+    expect(mockDispatch).toHaveBeenCalledWith(isLoading(false));
   });
+
 });
