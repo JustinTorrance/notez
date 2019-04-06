a1e96cd38d4c33a7e8f414bc99d468f5ad13e53b
“Kimberly <kimberly.achkio@gmail.com>
2019-04-06 12:42:14 -0600

    Test calls dispatch with getNotes on fetchNotes thunk
    Co-authored-by: Justin Torrance <justin.torrance91@gmail.com>
    Co-authored-by: Joshua Lavarine <joshua@lavarine.com>

--- src/thunks/__tests__/fetchNotes.js
+++ src/thunks/__tests__/fetchNotes.js
@@ -4,15 +4,30 @@ import { getNotes, isLoading } from '../../actions';
 describe('fetchNotes', () => {
   let mockUrl;
   let mockDispatch;
+  let mockNotes;
 
   beforeEach(() => {
     mockUrl = 'www.somenotes.com';
     mockDispatch = jest.fn();
+    mockNotes = [
+      { id: '1', title: 'fakeTitle', body: [{id: 1, text: 'faketext'}] },
+      { id: '2', title: 'fakeTitle2', body: [{id: 2, text: 'faketext2'}] }
+    ];
   });
 
   it('calls dispatch with isLoading', () => {
     const thunk = fetchNotes(mockUrl);
     thunk(mockDispatch);
     expect(mockDispatch).toHaveBeenCalledWith(isLoading(true))
+  });
+
+  it('calls dispatch with getNotes if the response is ok', async () => {
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+      ok: true,
+      json: () => Promise.resolve(mockNotes)
+    }));
+    const thunk = fetchNotes(mockUrl);
+    await thunk(mockDispatch);
+    expect(mockDispatch).toHaveBeenCalledWith(getNotes(mockNotes))
   });
 });
