f33d603645dae6bd29f677478eec3e4633854e71
Kim Myers <43019784+kimmichurri@users.noreply.github.com>
2019-04-06 18:19:20 -0600

    Update Card.js

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import { mapDispatchToProps } from '../../containers/CardContainer/CardContainer';
 import { connect } from 'react-redux';
 import { deleteNote } from '../../thunks/deleteNote';
 
