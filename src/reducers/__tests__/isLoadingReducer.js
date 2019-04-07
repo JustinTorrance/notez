e21753afb5d83c428e880928e20e7e3e14def246
“Kimberly <kimberly.achkio@gmail.com>
2019-04-06 19:26:23 -0600

    Complete testing for isLoadingReducer

---
+++ src/reducers/__tests__/isLoadingReducer.js
@@ -0,0 +1,18 @@
+import { isLoadingReducer } from '../isLoadingReducer';
+import * as actions from '../../actions';
+
+describe('isLoadingReducer', () => {
+  const state = false;
+  it('should return default state', () => {
+    const action = {};
+    const results = isLoadingReducer(state, action);
+    expect(results).toEqual(state);
+  });
+
+  it('should return state with boolean of true', () => {
+    const boolean = true;
+    const action = actions.isLoading(boolean);
+    const results = isLoadingReducer(state, action);
+    expect(results).toEqual(boolean);
+  })
+});
