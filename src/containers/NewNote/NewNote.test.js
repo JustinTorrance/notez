52ac4726f3e2cf449297d57f46fbdeccb60aefe0
“Kimberly <kimberly.achkio@gmail.com>
2019-04-09 17:18:01 -0600

    Test default state and snapshot for NewNote.js

--- src/containers/NewNote/NewNote.test.js
+++ src/containers/NewNote/NewNote.test.js
@@ -1,8 +1,27 @@
 import { NewNote, mapDispatchToProps } from './NewNote';
 jest.mock('../../thunks/addNote');
+import React from 'react';
+import { shallow } from 'enzyme';
 
 describe('NewNote', () => {
-  const addNote = jest.fn()
+  const addNote = jest.fn();
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<NewNote addNote={addNote} />)
+  });
+
+  it('should have proper default state', () => {
+    expect(wrapper.state()).toEqual({
+      inputs: [],
+      title: '',
+      listItems: []
+    });
+  });
+
+  it('should match the snapshot', () => {
+    expect(wrapper.debug()).toMatchSnapshot();
+  });
     
   describe('mapDispatchToProps', () => {
     it('should call dispatch when using a function from mapDispatchToProps', () => {
