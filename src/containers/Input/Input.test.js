9011d81718489166a56e40c5ce16e5498801c460
Justin Torrance <justin.torrance91@gmail.com>
2019-04-09 20:45:59 -0600

    Test handleChange function

--- src/containers/Input/Input.test.js
+++ src/containers/Input/Input.test.js
@@ -5,6 +5,17 @@ import Input from './Input.js';
 describe('Input', () => {
   it('should match the snapshot', () => {
     const wrapper = shallow(<Input/>)
-    expect(wrapper).toMatchSnapshot()
+    expect(wrapper.debug()).toMatchSnapshot()
+  })
+
+  it('should set state to the values of form inputs', () => {
+    const wrapper = shallow(<Input />);
+    const mockedEvent = {
+      target: {
+        value: 'tacos'
+      }
+    }
+    wrapper.instance().handleChange(mockedEvent)
+    expect(wrapper.state('text')).toEqual('tacos')
   })
 })
