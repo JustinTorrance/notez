6862facf366db9389b2f24c1bedc9697e30b709a
“Kimberly <kimberly.achkio@gmail.com>
2019-04-10 08:05:54 -0600

    Test toggleCompleted and deleteListItem

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -7,9 +7,9 @@ describe('actions', () => {
       type: 'CREATE_NOTE',
       note,
     }
-    const result = actions.createNote(note)
-    expect(result).toEqual(expected)
-  })
+    const result = actions.createNote(note);
+    expect(result).toEqual(expected);
+  });
 
   it('should return a type of GET_NOTES with an array of notes', () => {
     const notes = [{ title: 'walk dog', id: 1, listItems: [{id: 9, text: 'find leash'}]}];
@@ -17,26 +17,46 @@ describe('actions', () => {
       type: 'GET_NOTES',
       notes,
     }
-    const result = actions.getNotes(notes)
-    expect(result).toEqual(expected)
-  })
+    const result = actions.getNotes(notes);
+    expect(result).toEqual(expected);
+  });
 
   it('should return a type of IS_LOADING with boolean', () => {
     const expected = {
       type: 'IS_LOADING',
       isLoading: false
     }
-    const result = actions.isLoading(false)
-    expect(result).toEqual(expected)
-  })
+    const result = actions.isLoading(false);
+    expect(result).toEqual(expected);
+  });
 
   it('should return a type of DELETE_NOTE with an id', () => {
-    const id = 4
+    const id = 4;
     const expected = {
       type: 'DELETE_NOTE',
-      id,
+      id
     }
-    const result = actions.deleteNoteAction(id)
-    expect(result).toEqual(expected)
-  })
-})
+    const result = actions.deleteNoteAction(id);
+    expect(result).toEqual(expected);
+  });
+
+  it('should return a type of TOGGLE_COMPLETED with an id', () => {
+    const id = 7;
+    const expected = {
+      type: 'TOGGLE_COMPLETED',
+      id
+    }
+    const result = actions.toggleCompleted(id);
+    expect(result).toEqual(expected);
+  });
+
+  it('should return a type of DELETE_LIST_ITEM with an id', () => {
+    const id = 9;
+    const expected = {
+      type: 'DELETE_LIST_ITEM',
+      id
+    }
+    const result = actions. deleteListItem(id);
+    expect(result).toEqual(expected);
+  });
+});
