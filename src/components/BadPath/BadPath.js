497913b97cec06c81011e38ce5e4bb90b63b244d
Justin Torrance <justin.torrance91@gmail.com>
2019-04-10 07:38:39 -0600

    Create and test BadPath component

---
+++ src/components/BadPath/BadPath.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const BadPath = () => {
+  return (
+    <div>
+      <h2>404</h2>
+      <p>
+        This page does not exist
+      </p>
+    </div>
+  )
+}
+
+export default BadPath;
